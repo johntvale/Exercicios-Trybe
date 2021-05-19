780075102a729d1fc19beca5f0852b0a1cd61509
johntvale <johntvale@gmail.com>
2021-05-19 19:03:09 -0400

    exercicio 5 resolvido.

---
+++ Bloco_4/Dia_1/exercicio_5.js
@@ -0,0 +1,11 @@
+let num1=2;
+let num2=150;
+let num3=60;
+
+teste=num1+num2+num3;
+
+if((teste>=180) & (num1>0) & (num2>0) & (num3>0)){
+    console.log("triangulo Válido.");
+} else {
+    console.log("Triangulo inválido");
+}
