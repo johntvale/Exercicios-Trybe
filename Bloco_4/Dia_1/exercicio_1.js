7842c216104d8c125918052d0537b1a4c40a7811
johntvale <johntvale@gmail.com>
2021-05-19 18:23:29 -0400

    exercicio 1 resolvido.

---
+++ Bloco_4/Dia_1/exercicio_1.js
@@ -0,0 +1,25 @@
+let adicao = null;
+let subtracao = null;
+let multiplicacao =null;
+let divisao = null;
+let modulo = null;
+let a=null;
+let b=null;
+
+a=20;
+b=5;
+
+adicao=(a+b);
+console.log(adicao);
+
+subtracao=(a-b);
+console.log(subtracao);
+
+multiplicacao=(a*b);
+console.log(multiplicacao);
+
+divisao=(a/b);
+console.log(divisao);
+
+modulo=(a%b);
+console.log(modulo);
