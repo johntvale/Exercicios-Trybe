9589b9589dff62296175801ce10a66bf869c7228
johntvale <johntvale@gmail.com>
2021-05-19 18:00:57 -0400

    nova branch de exercicios, dia 4.1

---
+++ Bloco_4/Dia_1/Exemplos/scripts-parte2.js
@@ -0,0 +1,13 @@
+const name = 'John';
+const birthCity = 'Manaus';
+let birthYear ='1992';
+
+console.log(name, birthCity, birthYear);
+
+birthYear = 2030;
+
+console.log(birthYear);
+
+birthCity = 'Belo Horizonte';
+
+console.log(birthCity);
