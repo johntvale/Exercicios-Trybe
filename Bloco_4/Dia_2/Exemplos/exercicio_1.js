b5b5ffb8c01a8ff284a7c044d6606e8f97a7f0b8
johntvale <johntvale@gmail.com>
2021-05-20 14:25:33 -0400

    exemplos de comandos com array

---
+++ Bloco_4/Dia_2/Exemplos/exercicio_1.js
@@ -0,0 +1,7 @@
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let menuServices = menu[1];
+
+console.log(menuServices);
+
+//menu[1];
+// obtém o valor do array menu na posição 1.
---
+++ Bloco_4/Dia_2/Exemplos/exercicio_2.js
@@ -0,0 +1,7 @@
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let indexOfPortfolio = menu.indexOf('Portfólio');
+
+console.log(indexOfPortfolio);
+
+// menu.indexof('Portfólio');
+// obtém a posição do item 'Portfólio' no array menu
