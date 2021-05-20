511fe8bea924550769fc3a0f60b434d4a6982b1e
johntvale <johntvale@gmail.com>
2021-05-20 17:12:21 -0400

    exemplos de exercicios com comando For Of

---
+++ Bloco_4/Dia_2/Exemplos/exercicio_5.js
@@ -0,0 +1,24 @@
+//exercícios for of
+
+// let numeros = [1,2,3,4,5];
+// for(let numero of numeros) {
+//   console.log(numero);
+// }
+
+//exibe todos os números do array numeros, linha a linha.
+//mas não altera os dados do array original.
+//*****************************************
+
+//outro exemplo, agora com string.
+
+
+let word = 'Hello';
+for (let letter of word) {
+  console.log(letter);
+}
+// resultado:
+// "H"
+// "e"
+// "l"
+// "l"
+// "o"
