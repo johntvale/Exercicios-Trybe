a7e4bd74dbc065f67c460a70cced9a3de60fe8ec
johntvale <johntvale@gmail.com>
2021-05-19 23:22:07 -0400

    exercicio 7 resolvido.

---
+++ Bloco_4/Dia_1/exercicio_7.js
@@ -0,0 +1,17 @@
+let nota = 101;
+
+if(nota>=90 & nota<=100){
+    console.log('A');
+} else if (nota>=80 & nota<90){
+    console.log('B');
+} else if (nota>=70 & nota<80){
+    console.log('C');
+} else if (nota >=60 & nota<70){
+    console.log('D');
+} else if (nota >=50 & nota<60){
+    console.log('E');
+} else if(nota >=0 & nota<50){
+    console.log('F');
+} else if(nota < 0 || nota > 100){
+    console.log('Nota inválida. Digite um número entre 0 e 100.');
+}
