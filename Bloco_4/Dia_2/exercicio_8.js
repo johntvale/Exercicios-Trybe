53e812307c5a0060ababebc8caeaf11845c60b6b
johntvale <johntvale@gmail.com>
2021-05-20 20:52:20 -0400

    Atualizaçao e inclusao dos novos exercicios sobre array e for

---
+++ Bloco_4/Dia_2/exercicio_8.js
@@ -0,0 +1,8 @@
+// Utilizando for , crie um array que vá de 1 até 25 e imprima o resultado;
+
+let lista=[];
+
+for (let count=0; count<25; count+=1){
+    lista[count]=(count+1);
+}
+console.log(lista);
---
+++ Bloco_4/Dia_2/exercicio_9.js
@@ -0,0 +1,11 @@
+// Utilizando o array criado no exercício anterior imprima
+// o resultado da divisão de cada um dos elementos por 2 .
+
+let lista=[];
+
+for (let count=0; count<25; count+=1){
+    lista[count]=(count+1);
+}
+for (let count=0; count<25; count+=1){
+    console.log(lista[count]/2);
+}
