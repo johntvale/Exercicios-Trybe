8828aa2704fd7dd5855059521c511e6dcf94071c
johntvale <johntvale@gmail.com>
2021-05-21 18:00:17 -0400

    Exercicio2 - criando Triangulo feito de asteriscos.

--- Bloco_4/Dia_3/exercicio1.js
+++ Bloco_4/Dia_3/exercicio1.js
@@ -1,10 +1,10 @@
 // 1- Para o primeiro exercício de hoje, faça um programa que, dado um valor n qualquer, seja n > 1 , imprima na tela um quadrado feito de asteriscos de lado de tamanho n . Por exemplo:
 
-// criar uma variável "n" para receber o número de lados de um quadrado.
+// criar uma variável "n" para receber o número de lados de um quadrado, e variável listaDeAsteriscos para criar um dos lados do quadrado.
 let n=10;
 let listaDeAsteriscos=[];
 
-// criar um comando For para receber '*' na variavel 'listaDeAsteriscos' e criar uma lista de '*'.
+// criar um comando For para receber '*' na variavel 'listaDeAsteriscos'.
 for(let index=0; index<n; index+=1){	
 	if(n>1){
 		listaDeAsteriscos=listaDeAsteriscos+'*';
