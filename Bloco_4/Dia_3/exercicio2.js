b76345dc315cdfa8c8c2c05d3d976708812c1cd0
johntvale <johntvale@gmail.com>
2021-05-24 14:21:21 -0400

    atualizando pasta de exercicios

--- Bloco_4/Dia_3/exercicio2.js
+++ Bloco_4/Dia_3/exercicio2.js
@@ -2,10 +2,10 @@
 
 //criar uma variável 'n' para receber o tamanho da base do triangulo;
 //criar uma viarável para receber * e criar formato do triangulo.
-let n=100;
+let n=10;
 let listaDeAsteriscos=[];
 
-// comando for para receber * e imprimir linha a linha em uma lista, criando o trangulo.
+// comando for para receber * e imprimir linha a linha em uma lista, criando o triangulo.
 for(let index=0; index<n; index+=1){
 
 	//verifica se é maior que 1 para poder executar o código.
@@ -15,5 +15,6 @@ for(let index=0; index<n; index+=1){
 	}
 	else{
 		console.log('Número menos ou igual a 1.');
+		break;
 	}
 }
