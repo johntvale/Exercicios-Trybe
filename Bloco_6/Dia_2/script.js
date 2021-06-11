ba6ab52af0c9a74d57ae4b7734614a9806279906
johntvale <johntvale@gmail.com>
2021-06-11 13:47:01 -0400

    formulario refatorado com Bootstrap para CSS e JS.

--- Bloco_6/Dia_2/script.js
+++ Bloco_6/Dia_2/script.js
@@ -1,13 +1,10 @@
 const dropEstado = document.getElementById('dropdown-estado');
 const siglas = ['AC', 'AL', 'AP', 'AM', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MT', 'MS', 'MG', 'PA', 'PB', 'PR', 'PE', 'PI', 'RJ', 'RN', 'RS', 'RO', 'RR', 'SC', 'SP', 'SE', 'TO'];
 const estados = ['Acre', 'Alagoas', 'Amapá', 'Amazonas', 'Bahia', 'Ceará', 'Distrito Federal', 'Espírito Santo', 'Goiás', 'Maranhão', 'Mato Grosso', 'Mato Grosso do Sul', 'Minas Gerais', 'Pará', 'Paraíba', 'Paraná', 'Pernambuco', 'Piauí', 'Rio de Janeiro', 'Rio Grande do Norte', 'Rio Grande do Sul', 'Rondônia', 'Roraima', 'Santa Catarina', 'São Paulo', 'Sergipe', 'Tocantins'];
-console.log(siglas);
-console.log(estados);
 
 for (let index = 0; index < siglas.length; index += 1) {
   const option = document.createElement('Option');
   option.setAttribute(className = 'value', value = siglas[index]);
   option.innerText = estados[index];
   dropEstado.appendChild(option);
-  console.log(dropEstado[index]);
 }
