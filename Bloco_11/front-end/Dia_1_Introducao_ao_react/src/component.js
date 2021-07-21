eeda8776705340f6f8fec5b9ba304d9c0d207b9b
johntvale <johntvale@gmail.com>
2021-07-20 23:02:43 -0400

    Aula 11.1, Introduçao ao React

---
+++ Bloco_11/front-end/Dia_1_Introducao_ao_react/src/component.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class MeuNome extends Component {
+  render() {
+    const elements = (
+      <div>
+        <h1>John Torres do Vale</h1>
+        <p>pequeno exemplo de parágrafo.</p>
+      </div>
+      );
+    return elements;
+  }
+}
+
+export default MeuNome;
